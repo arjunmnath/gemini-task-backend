c79fd8ad9dbdd83e47518ecfe21ca5e62b92e941
Arjun Manjunath <arjun@Arjuns-MacBook-Air.local>
2024-01-24 12:00:53 +0530

    fix: EOL error

--- api/index.py
+++ api/index.py
@@ -47,50 +47,49 @@ model = genai.GenerativeModel(model_name="gemini-pro",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
+
 convo = model.start_chat(history=[
-    {
-        "role": "user",
-        "parts": ["you are a medical chatbot"]
-        },
-    {
-        "role": "model",
-        "parts": ["ok"]
-        },
-    {
-        "role": "user",
-        "parts": ["i have Sore throat, Cough, Congestion, Slight body aches or a mild headache, Sneezing, Low-grade fever, Generally feeling unwell (malaise)"]
-        },
-    {
-        "role": "model",
-        "parts": ["seems like you have a common cold, Most people recover on their own within two weeks. so take rest and be hydrated"]
-        },
-    {
-        "role": "user",
-        "parts": ["i have Lost interest,Increased fatigue,sleep problem, Anxiety, change in weight, Looking at death, what is my problem"]
-        },
-    {
-        "role": "model",
-        "parts": ["Looks like you suffering from depression, you must visit a medical professional as soon as possible."]
-        },
-    {
-        "role": "user",
-        "parts": ["i have coughing, thightness in chest, and fatigue what am i having"]
-        },
-    {
-        "role": "model",
-        "parts": ["It's likely that you have pneumonia. This is a lung infection that can cause coughing, tightness in the chest, and fatigue. It can also cause fever, chills, and shortness of breath. You should see a doctor as soon as possible so that you can get treatment."]
-        },
-    {
-        "role": "user",
-        "parts": ["Which medicine to apply for Cuts?"]
-        },
-    {
-        "role": "model",
-        "parts": [
-            "Wash the cut properly to prevent infection and stop the bleeding by applying pressure for 1-2minutes until bleeding stops. Apply Petroleum Jelly to make sure that the wound is moist for quick healing. Finally cover the cut with a sterile bandage. Pain relievers such as acetaminophen can be applied."]
-        },
-       }
-    ])
+  {
+    "role": "user",
+    "parts": ["you are a medical chatbot"]
+  },
+  {
+    "role": "model",
+    "parts": ["ok"]
+  },
+  {
+    "role": "user",
+    "parts": ["i have Sore throat, Cough, Congestion, Slight body aches or a mild headache, Sneezing, Low-grade fever, Generally feeling unwell (malaise)"]
+  },
+  {
+    "role": "model",
+    "parts": ["i have Sore throat, Cough, Congestion, Slight body aches or a mild headache, Sneezing, Low-grade fever, Generally feeling unwell (malaise)"]
+  },
+  {
+    "role": "user",
+    "parts": ["\"i have Lost interest,Increased fatigue,sleep problem, Anxiety, change in weight, Looking at death, what is my problem"]
+  },
+  {
+    "role": "model",
+    "parts": ["Looks like you suffering from depression, you must visit a medical professional as soon as possible."]
+  },
+  {
+    "role": "user",
+    "parts": ["i have coughing, thightness in chest, and fatigue what am i having"]
+  },
+  {
+    "role": "model",
+    "parts": ["It's likely that you have pneumonia. This is a lung infection that can cause coughing, tightness in the chest, and fatigue. It can also cause fever, chills, and shortness of breath. You should see a doctor as soon as possible so that you can get treatment."]
+  },
+  {
+    "role": "user",
+    "parts": ["Which medicine to apply for Cuts?"]
+  },
+  {
+    "role": "model",
+    "parts": ["Wash the cut properly to prevent infection and stop the bleeding by applying pressure for 1-2minutes until bleeding stops. Apply Petroleum Jelly to make sure that the wound is moist for quick healing. Finally cover the cut with a sterile bandage. Pain relievers such as acetaminophen can be applied."]
+  },
+])
 @app.route("/")
 @cross_origin()
 def main():
